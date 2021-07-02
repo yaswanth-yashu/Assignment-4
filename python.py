e842be979b9105b247cb6526f413390298f405df
yaswanth-yashu <86795414+yaswanth-yashu@users.noreply.github.com>
2021-07-02 07:29:48 +0530

    Update and rename python.py to Assignment-4

--- python.py
+++
@@ -1,9 +0,0 @@
-#plane at altitude 1000ft
-feet=1000
-
-if feet<=(1000):
-  print("land the palne")
-elif feet<4500:
-  print("come down to 1000ft")
-elif feet<6000:
-  print("go around and try layer")
